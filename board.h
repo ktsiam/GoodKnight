2d74e5cb7a50d70402420d266a237eb4f2bdacf8
ktsiam <ktsiampouris@gmail.com>
2018-02-10 14:53:34 -0500

    simplify board

--- board.h
+++ board.h
@@ -29,34 +29,6 @@ protected:
 
         //move history
         std::stack<Move> history;
-        
-private:
-        
-        //move generators
-        void castling_gen();
-        void king_move_gen();
-        void knight_move_gen();
-        void pawn_move_gen();
-        void rook_move_gen();
-        void bishop_move_gen();
-        void queen_move_gen();
-
-        //static generator helpers
-        void general_move_gen(BB origin, Piece pce, BB squares);
-        void pawn_move       (BB origin, BB dest);
-        void pawn_capture    (BB origin, BB dest);        
-        void promotion_gen   (BB origin, BB dest, bool quiet);
-        BB   rel_shift_up  (BB b, uint8_t shift);
-        BB   rel_shift_down(BB b, uint8_t shift);
-
-        //sliding generator helpers
-        void rank_move_gen    (BB origin, Piece pce);
-        void file_move_gen    (BB origin, Piece pce);
-        void diagonal_move_gen(BB origin, Piece pce);
-        void antidiag_move_gen(BB origin, Piece pce);
-        uint8_t byte_bb_gen      (uint8_t orig, uint8_t occup);
-        uint8_t byte_bb_gen_left (uint8_t orig, uint8_t occup);
-        uint8_t byte_bb_gen_right(uint8_t orig, uint8_t occup);
 };
 
 
