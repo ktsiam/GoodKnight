b2d3c587d2a2c8db4fe63f47a258135a534fe71d
ktsiam <ktsiampouris@gmail.com>
2018-02-12 11:24:46 -0500

    implementing trivial evaluator

--- evaluator.cpp
+++ evaluator.cpp
@@ -4,8 +4,19 @@ Evaluator::Evaluator()
 {
 }
 
-int Evaluator::evaluate()
+int Evaluator::evaluate() 
 {
         //TODO
-        return 0;
+        return                
+                900 * pop_count(pieces[clr  ][QUEEN ]) +
+                500 * pop_count(pieces[clr  ][ROOK  ]) +
+                300 * pop_count(pieces[clr  ][BISHOP]) +
+                300 * pop_count(pieces[clr  ][KNIGHT]) +
+                100 * pop_count(pieces[clr  ][PAWN  ]) -
+                900 * pop_count(pieces[clr^1][QUEEN ]) -
+                500 * pop_count(pieces[clr^1][ROOK  ]) -
+                300 * pop_count(pieces[clr^1][BISHOP]) -
+                300 * pop_count(pieces[clr^1][KNIGHT]) -
+                100 * pop_count(pieces[clr^1][PAWN  ]);
+
 }
