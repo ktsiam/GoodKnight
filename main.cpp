77b2575ede121d8133e55ce7cb362ed5fe52319d
ktsiam <ktsiampouris@gmail.com>
2018-02-13 13:16:56 -0500

    now allows -v option

--- main.cpp
+++ main.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 
-#include "test_board.h"
+#include "interface.h"
 
 unsigned TIME_TAKEN;
 unsigned MOVE_COUNT = 0;
 const uint8_t DEPTH = 6;
 
-[[noreturn]] int main() 
+[[noreturn]] int main(int argc, char *argv[]) 
 {
-        Test_board b;
-        b.print();
-
+        Interface inter;
         std::string move;
-        while(std::cin >> move){
-                b.custom_move(move);
-                std::cout << "LOOKED at "     << MOVE_COUNT/1000
-                          << "K POSITIONS in\n" << TIME_TAKEN/1000
-                          << "ms\n";
-                MOVE_COUNT = 0;
+        bool verbose = (argc == 2) && (!strcmp(argv[1], "-v"));
+        
+        while(std::cin >> move){                
+                inter.custom_move(move);
+                
+                if (MOVE_COUNT != 0 && verbose) {
+                        std::cout << "LOOKED at "
+                                  << MOVE_COUNT/1000
+                                  << "K POSITIONS in\n"
+                                  << TIME_TAKEN/1000
+                                  << "ms\n";
+                        MOVE_COUNT = 0;
+                }
         }
 }
